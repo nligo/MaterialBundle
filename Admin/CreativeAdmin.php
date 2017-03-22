9719ab60ebd02cdd6b51232bb2728cb6957fb3d5
coffey <coffey.gao@appcoachs.com>
2017-03-22 14:26:29 +0800

    creative list

--- Admin/CreativeAdmin.php
+++ Admin/CreativeAdmin.php
@@ -21,27 +21,67 @@ use Sonata\AdminBundle\Form\FormMapper;
  */
 class CreativeAdmin extends BaseAdmin
 {
-    protected $baseRouteName = 'appcoachs_material_creavite';
     // Fields to be shown on create/edit forms
     protected function configureFormFields(FormMapper $formMapper)
     {
 
         $formMapper
             ->add('name')
-            ->add('status')
             ->add('media')
+            ->add('adgroup','choice',array(
+                'required'=>false
+            ))
+            ->add('campaign')
             ->add('type');
     }
+
+    //  input search condition
     protected function configureDatagridFilters(DatagridMapper $datagridMapper)
     {
+        $datagridMapper->add('name',null,array('label'=>'Creative'));
+        $datagridMapper->add('campaign.name',null,array('label'=>'Campaign'));
+        $datagridMapper->add('adgroup.name',null,array('label'=>'Adgroup'));
     }
 
+    //  list fileds
     protected function configureListFields(ListMapper $listMapper)
     {
         $listMapper
-            ->add('name', 'string', array('label' => 'Name', 'sortable' => false))
-            ->add('status', 'string', array('label' => 'Status', 'sortable' => false))
-            ->add('type', 'string', array('label' => 'Ad Type', 'sortable' => false))
-            ->add('createdAt', 'date', array('label' => 'Created At'));
+            ->add('status', 'string',
+                array(
+                    'label' => 'Status'
+                )
+            )
+            ->add('campaign.name', 'url',
+                array(
+                'label' => 'Campaign Name',
+                'route' => array('name' => 'appcoachs_campaign_list')
+                )
+            )
+            ->add('adgroup.name','url',array(
+                'label' => 'Adgroup',
+                )
+            )
+            ->add('media.name', 'url', array(
+                'label' => 'Active Video Media',
+                'route' => array('name' => 'admin_appcoachs_manage_media_list')
+                )
+            )
+            ->add( 'name', null,
+                array(
+                    'label' => 'Creative',
+                    'route' => array( 'name' => 'edit')
+                )
+            )
+            ->add('createdAt','date',
+                array('label' => 'Created At')
+            )
+            ->add('_action', 'actions', array(
+                'actions' => array(
+                    'edit' => array(),
+                    'delete' => array(),
+                )
+            ))
+        ;
     }
 }
